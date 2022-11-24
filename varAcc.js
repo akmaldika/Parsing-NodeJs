f12a15145b9783edbf2e203848639994ebe7bab8
razzanYoni <16521378@mahasiswa.itb.ac.id>
2022-11-24 22:57:29 +0700

    tambah kasus

--- varAcc.js
+++ varAcc.js
@@ -1,3 +1,101 @@
+const longString = 'This is a very long string which needs \
+to wrap across multiple lines because \
+otherwise my code is unreadable.'
+
+let CobaString = 'This is a very long string which needs ' +
+                   'to wrap across multiple lines because ' +
+                   'otherwise my code is unreadable.'
+
+const TBFOKelaramin = -(a ** b)
+
+let hmam = (-a) ** b
+
+if ((true) || (false)) {
+
+}
+
+for (let i = 0; i < 5; ++i) {
+    console.log(i);
+  }
+
+if ((manJadda = Wajadda)){
+    // harusnya bisa karena assignment dan ada 2 bracket
+}
+
+if (SamaLiteralDanTipe === samaLiteralDanTipe){
+    // harusnya bisa karena boolean
+}
+
+function tes(lur) {
+    function tes2() {
+        return lur;
+    }
+}
+
+function cheer(score) {
+    if (score === 147) {
+      return 'Maximum!';
+    }
+    if (score > 100) {
+      return 'Century!';
+    }
+  }
+function f(arg) {
+    arg = 'foo';
+  }
+  
+  function g(arg) {
+    let bar = 'foo';
+  }
+
+let hmm, y
+
+function greet(greeting) {
+    return greeting
+}
+
+function square(number) {
+    return number * number
+}
+
+const diAssignH3h3 = "lur"
+
+const obj = {
+    a: 1,
+    b: { myProp: 2 },
+    c: 3
+  }
+
+(function () {
+    if (true) {
+      return false
+    }
+  })
+
+const charge = function () {
+    if (sunny) {
+      useSolarCells()
+    } else {
+      promptBikeRide()
+    }
+  }
+
+const list = [1, 2]
+
+const instruments = [
+ 'Ukulele',
+ 'Guitar',
+ 'Piano'
+]
+
+const data = [{ foo: 'bar' }, { bar: 'foo' }]
+
+let am = '"Java" + "Script" = "' + 'Java' + 'Script"'
+
+let faa = "Tom\ 's bar"
+
+var he = "Tom's bar"
+
 let A,B
 A = 1
 B = 2
@@ -12,7 +110,7 @@ const car = {type:"Fiat", model:"500", color:"white"}
 
 agao = true
 
-abad + gsfgs
+bermain_main = abad + gsfgs
 
 main = function berMain(ibu, ayah) {
   return 1
