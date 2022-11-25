d7d81a4a12404969b1a80aed70c40b5ce255e1d8
4kmalm <16521070@mahasiswa.itb.ac.id>
2022-11-25 22:38:03 +0700

    add grammar all posible cons, var, let

---
+++ test/Rejected.js
@@ -0,0 +1,153 @@
+// const longString = 'This is a very long string which needs
+//                     to wrap across multiple lines because
+//                     otherwise my code is unreadable.'
+// harusnya gak di-acc
+                    
+
+// let pamping_lemah = -a ** b // tokenizer failure
+
+// if (true) || (false) {
+//     // gabisa karena kurang kurung
+// }
+
+
+// function cheer(score) {
+//     if (score === 147)
+//       return 'Maximum!'
+//     }
+//     if (score > 100) {
+//       return 'Century!'
+//     }
+// } // cbo ilang kalo kita harus CBO CBC ini harusnya error tapi kalo bisa ga pake cbo harus dihandle lagi :v
+
+// function f(arg){
+//     let arg = 'mak' // gaboleh dikasi let lagi kalo mau let ganti identifier
+// }
+
+// let arrGaAdaKurung = 1,2,3
+
+// const z, qt = 1, 4
+
+// const mkh, = "salah-salah"
+// const debugger = "whoop" // nama gaboleh debugger karena reserved keyword
+
+// const = "maak"
+
+// function log({ obj: "value"}) {
+//     console.log(arg)
+//   }
+
+// function greet("Howdy") {
+//     return greeting
+// }
+
+// function square(3) {
+//     return number * number
+// }
+
+// const gaDiAssign
+// const obj = {
+//     b: { myProp: 2 }
+//     a: 1,
+//     c: 3
+//   }
+// (function () { if (true) { return false; } )
+
+// const charge = function () {
+//     if (sunny) {
+//       useSolarCells()
+//     } else {
+//       promptBikeRide()
+//   }
+
+// const list = [1, 2,
+
+// const instruments = [
+//   'Ukulele',
+//   'Guitar',
+//   'Piano'
+// }
+
+// const data = [{ foo: 'bar' } { bar: 'foo' }]
+
+// if (Math.PI < 3 {
+//   }
+
+// let ab = '"Java" + "Script" = \"' + 'Java' + 'Script\"'
+// mau ditambah petik satu juga masih error 
+
+// const foo = 'Tom's bar'
+
+// const lahKok = { Key = "value" }
+
+// try {
+//     i = 0
+// }
+
+// else duluan
+// else () {}
+
+// else if duluan
+// else if () {
+
+// }
+
+// if (makan) {
+
+// if makan + minum {
+// }
+
+// if (makan) && (minum) {
+//     1
+// }
+
+
+// afad afaf = 1 // harusnya gabisa
+
+// afa 2 = 1
+
+var let = 1 // let boleh dijadikan identifier tapi gaboleh bound let let atau let var
+
+// (((i++)++)++)
+
+
+// for (let i = 0; i < 5,; ++i) {
+//     // gabisa karena setelah 5 ada koma
+//   }
+
+// y = 3 + *5
+
+// const e = 1
+
+// throw e
+
+
+// try {
+
+// }
+
+// else duluan
+// else () {}
+
+// else if duluan
+// else if () {
+
+// }
+
+// if (makan) {
+
+// if makan + minum {
+// }
+
+// if (makan) && (minum) {
+//     1
+// }
+
+
+// afad afaf = 1
+
+// afa 2 = 1
+
+// let = 1
+
+// (((i++)++)++)
